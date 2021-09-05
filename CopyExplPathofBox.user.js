d6ef8fb879b41e813cda09d690699c0072091b45
Kevin <a645749023@gmail.com>
2021-09-05 19:02:39 +0900

    Fix problem with path can not be copied when menu button does not exist

--- CopyExplPathofBox.user.js
+++ CopyExplPathofBox.user.js
@@ -2,7 +2,7 @@
 // @name         Copy explorer path of Box folder
 // @description  Add a button on Box website that can copy explorer path of Box folder.
 // @namespace    https://github.com/kevinzch/Copy-explorer-path-of-Box-folder
-// @version      0.1
+// @version      0.2
 // @downloadURL  https://github.com/kevinzch/Copy-explorer-path-of-Box-folder/raw/main/CopyExplPathofBox.user.js
 // @author       Kevin
 // @include      https://app.box.com/folder/*
@@ -16,37 +16,53 @@
 
     const copyExplorerPath = () => {
         try {
-            let b1 = document.createElement('button');
-            b1.textContent = 'コピー';
-            let searchbar = document.querySelector('.header-search.prevent-item-deselection.HeaderSearch-isNewQuickSearch');
-            searchbar.appendChild(b1);
+            let copyBtn = document.createElement('button');
+            copyBtn.textContent = 'フォルダパスをコピー';
+            copyBtn.style.backgroundColor = '#4baf4f';
+            copyBtn.style.color = 'white';
+            copyBtn.style.borderRadius = '8px';
+            copyBtn.style.padding = '0px 20px';
 
-            b1.addEventListener('click', function(){
+            let searchbar = document.querySelector('.header-search.prevent-item-deselection.HeaderSearch-isNewQuickSearch');
+            searchbar.appendChild(copyBtn);
+
+            copyBtn.addEventListener('click', function(){
+                // Set menu button
                 let hiddenPathButton = document.querySelector('button.btn-plain.ItemListBreadcrumbOverflow-menuButton');
                 let topOfBreadcrumbList = document.querySelector('ol.ItemListBreadcrumb-list');
                 let breadcrumb = topOfBreadcrumbList.querySelectorAll('[class=ItemListBreadcrumb-listItem]');
                 let lastBreadcrumb = topOfBreadcrumbList.querySelector('.ItemListBreadcrumb-listItem.is-last>.ItemListBreadcrumb-currentItemTitle');
-                let text = 'Box\\';
+                let text = "Box\\";
 
-                hiddenPathButton.click();
-                setTimeout(function() {
-                    let hiddenList = document.querySelector('div.dropdown-menu-element.dropdown-menu-enabled');
-                    let list = hiddenList.querySelectorAll('[data-resin-target=openfolder]');
+                // Click menu button only if it exists
+                if ( hiddenPathButton ){
+                    hiddenPathButton.click();
 
-                    for (let item of list){
-                        if ( item.textContent != 'All Files' ){
+                    setTimeout(function() {
+                        let hiddenList = document.querySelector('div.dropdown-menu-element.dropdown-menu-enabled');
+                        let list = hiddenList.querySelectorAll('[data-resin-target=openfolder]');
+
+                        for (let item of list){
                             text += item.textContent + "\\";
                         }
-                    }
+                    },200);
 
+                    // Click again to hide menu
+                    hiddenPathButton.click();
+                }
+
+                setTimeout(function() {
                     for (let item of breadcrumb){
-                        text += item.textContent + '\\';
+                        text += item.textContent + "\\";
                     }
 
                     text += lastBreadcrumb.textContent;
+                    text = text.replace("All Files\\", "");
+                    text = text.replace("すべてのファイル\\", "");
                     navigator.clipboard.writeText(text);
-                    alert('エクスプローラーパスをコピーしました。');
-                },500);
+                    alert("下記のパスをコピーしました。\r\n" + text);
+                },200);
+
             })
         } catch (e) {
             setTimeout(() => {
